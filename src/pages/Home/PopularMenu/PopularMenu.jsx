10ca6b90f103222aab291486d5fd69862558e00d
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-10 22:13:07 +0600

    added popular menu in home page

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
   // state
@@ -21,6 +22,12 @@ const PopularMenu = () => {
         heading={"From Our Menu"}
         subHeading={"Popular Item"}
       ></SectionTitle>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-8">
+        {menu.map(item => (
+          <MenuItem item={item} key={item._id}></MenuItem>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,21 @@
+const MenuItem = ({ item }) => {
+  const { image, price, recipe, name } = item;
+  return (
+    <div className="flex space-x-4">
+      <img
+        className="w-[118px] h-[104px] rounded-r-[200px]  rounded-b-[200px]"
+        src={image}
+        alt={name}
+      />
+      <div>
+        <h3 className="text-[#151515] uppercase text-xl font-medium">
+          {name} -----------
+        </h3>
+        <p className="text-[#737373]">{recipe}</p>
+      </div>
+      <p className="text-[#BB8506]">${price}</p>
+    </div>
+  );
+};
+
+export default MenuItem;
