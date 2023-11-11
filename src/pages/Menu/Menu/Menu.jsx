e935292333c6c91c232acf46d23a26e810d52110
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-12 00:10:34 +0600

    added parallax effect in cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import coverImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -8,8 +9,12 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <Cover img={coverImg}></Cover>
-      <h1>His is menu</h1>
+      <Cover img={coverImg} coverTitle={"our menu"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={coverImg} coverTitle={"our menu"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={coverImg} coverTitle={"our menu"}></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,19 +1,28 @@
+import { Parallax } from "react-parallax";
+
 const Cover = ({ img, coverTitle }) => {
   return (
-    <div
-      className="hero h-[700px]"
-      style={{
-        backgroundImage: `url(${img})`,
-      }}
+    <Parallax
+      blur={{ min: -20, max: 20 }}
+      bgImage={img}
+      bgImageAlt="the dog"
+      strength={-190}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">{coverTitle}</p>
+      <div className="hero h-[700px]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase ">{coverTitle}</h1>
+            <p className="mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur odit ex voluptas aperiam recusandae nesciunt sint
+              itaque blanditiis, velit natus illo amet inventore omnis aut
+              temporibus quis id expedita eveniet.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
