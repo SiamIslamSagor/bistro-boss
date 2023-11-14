03191af7ff51f7acf374920c72a2b63fdfc00fff
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-15 04:57:36 +0600

    added a login page and added form

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/Navbar/NavBar";
 
 const Main = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname.includes("login");
+
   return (
     <div>
-      <NavBar></NavBar>
+      {isLoginPage || <NavBar></NavBar>}
       <Outlet></Outlet>
-      <Footer></Footer>
+      {isLoginPage || <Footer></Footer>}
     </div>
   );
 };
