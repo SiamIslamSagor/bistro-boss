1d279f58c3c149353b03b9290b0262d9eec35a4e
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-15 13:56:38 +0600

    added password validation in sign up page by using regex in react hook form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-simple-captcha";
 import useContextData from "../../hooks/useContextData";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
   // context data
@@ -49,88 +50,93 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
-        <div className="text-center md:w-1/2  lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <LoadCanvasTemplate></LoadCanvasTemplate>
-              </label>
-              <input
-                type="text"
-                ref={captchaRef}
-                name="captcha"
-                placeholder="type the captcha above"
-                className="input input-bordered"
-                required
-              />
-              <button
-                onClick={handleValidateCaptcha}
-                className="mt-2 btn btn-outline btn-xs"
-              >
-                Validate
-              </button>
-            </div>
-            <div className="form-control mt-6">
-              <input
-                disabled={isLoginDisable}
-                className="btn btn-primary"
-                type="submit"
-                value="Login"
-              />
-            </div>
-          </form>
-          <div className="text-center mb-10">
-            <p>
-              <small>
-                New Here? please{" "}
-                <Link className="text-blue-500 underline" to="/signUp">
-                  Sign Up
-                </Link>
-              </small>
+    <>
+      <Helmet>
+        <title>Bistro Boss | Log In</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="text-center md:w-1/2  lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
             </p>
+          </div>
+          <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate></LoadCanvasTemplate>
+                </label>
+                <input
+                  type="text"
+                  ref={captchaRef}
+                  name="captcha"
+                  placeholder="type the captcha above"
+                  className="input input-bordered"
+                  required
+                />
+                <button
+                  onClick={handleValidateCaptcha}
+                  className="mt-2 btn btn-outline btn-xs"
+                >
+                  Validate
+                </button>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  disabled={isLoginDisable}
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <div className="text-center mb-10">
+              <p>
+                <small>
+                  New Here? please{" "}
+                  <Link className="text-blue-500 underline" to="/signUp">
+                    Sign Up
+                  </Link>
+                </small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import useContextData from "../../hooks/useContextData";
 import { useForm } from "react-hook-form";
+import { Helmet } from "react-helmet-async";
 
 const SignUp = () => {
   // context data
@@ -22,91 +23,118 @@ const SignUp = () => {
 
   } */
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign Up now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Your Name</span>
-              </label>
-              {errors.name && (
-                <span className="text-sm text-red-500">Name is required *</span>
-              )}
-              <input
-                type="text"
-                name="name"
-                {...register("name", { required: true })}
-                placeholder="Your Name"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              {errors.email && (
-                <span className="text-sm text-red-500">
-                  Email is required *
-                </span>
-              )}
-              <input
-                type="email"
-                name="email"
-                {...register("email", { required: true })}
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              {/* {errors.password && (
+    <>
+      <Helmet>
+        <title>Bistro Boss | Sign Up</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign Up now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Name</span>
+                </label>
+                {errors.name && (
+                  <span className="text-sm text-red-500">
+                    Name is required *
+                  </span>
+                )}
+                <input
+                  type="text"
+                  name="name"
+                  {...register("name", { required: true })}
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    Email is required *
+                  </span>
+                )}
+                <input
+                  type="email"
+                  name="email"
+                  {...register("email", { required: true })}
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                {/* {errors.password && (
                 <span className="text-sm text-red-300">
                   Password is required *
                 </span>
               )} */}
-              {errors.password?.type === "required" && (
-                <p className="text-red-500">Password is must required **</p>
-              )}
-              <input
-                type="password"
-                name="password"
-                {...register("password", {
-                  required: true,
-                  minLength: 6,
-                  maxLength: 20,
-                })}
-                placeholder="password"
-                className="input input-bordered"
-              />
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-500">
+                    Password must be 6 characters *
+                  </p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p className="text-red-500">
+                    Password must be 20 characters *
+                  </p>
+                )}
+
+                <input
+                  type="text"
+                  name="password"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern: /(?=.*[A-Z])(?=.*[@$!%*?&])(?=.*[0-9])(?=.*[a-z])/,
+                    // /(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z@$!%*?&]/,
+                  })}
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                {errors.password?.type === "pattern" && (
+                  <p className="text-red-500">
+                    Password must be at least one lower case one uppercase one
+                    spacial character *
+                  </p>
+                )}
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </div>
+            </form>
+            <div className="text-center mb-10">
+              <p>
+                <small>
+                  All ready Registered? please{" "}
+                  <Link className="text-blue-500 underline" to="/login">
+                    Login
+                  </Link>
+                </small>
+              </p>
             </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
-            </div>
-          </form>
-          <div className="text-center mb-10">
-            <p>
-              <small>
-                All ready Registered? please{" "}
-                <Link className="text-blue-500 underline" to="/login">
-                  Login
-                </Link>
-              </small>
-            </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
