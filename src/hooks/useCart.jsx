224a6980347b370ad6684db3bba456c82689330c
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-16 19:05:14 +0600

    load cart info for specific user and show it in cart logo right side

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
+import useAuthInfo from "./useAuthInfo";
 
 const useCart = () => {
   // tan stack query
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuthInfo();
+  console.log(user);
   const { data: cart = [] } = useQuery({
-    queryKey: ["cartData"],
+    queryKey: ["cartData", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get("/carts");
+      const res = await axiosSecure.get(`/carts?email=${user?.email}`);
       return res.data;
     },
   });
