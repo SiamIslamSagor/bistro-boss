3a6b9ec7882b563f6aa3bd8f5156d909b0246e60
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-17 15:51:59 +0600

    added table in the dashboard my cart route

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types";
 import { NavLink, Outlet } from "react-router-dom";
 import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
 
 const Dashboard = props => {
   return (
     <div className="flex">
       {/* side navigation */}
-      <div className="w-64 min-h-full bg-yellow-700">
+      <div className="w-64 min-h-screen bg-[#D1A054]">
         <ul className="menu">
           <li>
-            <NavLink to="/dashboard/cart">
-              <FaShoppingCart></FaShoppingCart> My Cart
+            <NavLink to="/dashboard/userHome">
+              <FaHome></FaHome> User Home
             </NavLink>
           </li>
           <li>
@@ -19,24 +20,37 @@ const Dashboard = props => {
             </NavLink>
           </li>
           <li>
+            <NavLink to="/dashboard/cart">
+              <FaShoppingCart></FaShoppingCart> My Cart
+            </NavLink>
+          </li>
+          <li>
             <NavLink to="/dashboard/review">
               <FaCalendar></FaCalendar> Add a Review
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHome></FaHome> User Home
-            </NavLink>
-          </li>
+
           <li>
             <NavLink to="/dashboard/bookings">
               <FaHome></FaHome> My Bookings
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <IoMdMenu />
+              Menu
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1 bg-yellow-200">
+      <div className="flex-1 bg-yellow-50 p-10">
         <Outlet></Outlet>
       </div>
     </div>
