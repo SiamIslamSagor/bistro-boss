a17ca8c96f7939ceb586517c694c404c1ccff465
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-17 23:31:15 +0600

    send new google user info in the database

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,16 +1,36 @@
 import { FaGoogle } from "react-icons/fa";
 import useContextData from "../../hooks/useContextData";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
   //  context data
   const { loginWithGoogle } = useContextData();
+  const axiosPublic = useAxiosPublic();
+  const navigate = useNavigate();
 
   //  handler
   const handleGoogleLogin = () => {
     loginWithGoogle()
       .then(res => {
         console.log(res);
+
+        const userInfo = {
+          email: res?.user?.email,
+          name: res?.user?.displayName,
+        };
+
+        axiosPublic
+          .post("/users", userInfo)
+          .then(res => {
+            console.log(res.data);
+            navigate("/");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
         Swal.fire({
           title: "User Login Successfully!",
           icon: "success",
