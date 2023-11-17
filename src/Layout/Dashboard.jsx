683c4908a7db16278e50cad987d54ca30ef41a08
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-17 23:50:19 +0600

    create routes for adimg

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,42 +1,92 @@
 import PropTypes from "prop-types";
 import { NavLink, Outlet } from "react-router-dom";
-import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import {
+  FaBook,
+  FaBookmark,
+  FaCalendar,
+  FaEnvelope,
+  FaHome,
+  FaList,
+  FaShoppingCart,
+  FaUser,
+  FaUtensils,
+} from "react-icons/fa";
 import { IoMdMenu } from "react-icons/io";
 import useCart from "../hooks/useCart";
+import { useState } from "react";
 
 const Dashboard = props => {
   const [cart] = useCart();
+
+  // TODO: get is admin value from the database
+  const [isAdmin, setIsAdmin] = useState(true);
+
   return (
     <div className="flex">
       {/* side navigation */}
       <div className="w-64 min-h-screen bg-[#D1A054]">
         <ul className="menu">
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHome></FaHome> User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              <FaCalendar></FaCalendar> Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/cart">
-              <FaShoppingCart></FaShoppingCart> My Cart ({cart.length})
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-              <FaCalendar></FaCalendar> Add a Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/bookings">
-              <FaHome></FaHome> My Bookings
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHome></FaHome> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                  <FaUtensils></FaUtensils> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                  <FaList></FaList> Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaBook></FaBook> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/users">
+                  <FaUser></FaUser> All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/userHome">
+                  <FaHome></FaHome> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <FaCalendar></FaCalendar> Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  <FaShoppingCart></FaShoppingCart> My Cart ({cart.length})
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  <FaCalendar></FaCalendar> Add a Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaBookmark></FaBookmark> My Bookings
+                </NavLink>
+              </li>
+            </>
+          )}
+
+          {/* SHARED ROUTES */}
           <div className="divider"></div>
+
           <li>
             <NavLink to="/">
               <FaHome></FaHome> Home
@@ -46,6 +96,12 @@ const Dashboard = props => {
             <NavLink to="/order/salad">
               <IoMdMenu />
               Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/contact">
+              <FaEnvelope />
+              Contact
             </NavLink>
           </li>
         </ul>
