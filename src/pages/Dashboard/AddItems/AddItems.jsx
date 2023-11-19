af87961324b50e36bfd25f45f19d899146bf62ab
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-20 02:34:48 +0600

    add style in react form

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -11,23 +11,63 @@ const AddItems = () => {
         subHeading={"What's new?"}
       ></SectionTitle>
 
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} />
-          <select
-            {...register("category")}
-            className="select select-bordered w-full max-w-xs"
-          >
-            <option disabled selected>
-              Select a Category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="desserts">Desserts</option>
-            <option value="drinks">Drinks</option>
-            <option value="offered">Offered</option>
-          </select>
+      <div className="bg-[#F3F3F3] font-medium p-12 rounded-lg">
+        <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Recipe Name *</span>
+            </label>
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="Recipe Name"
+              className="input focus:outline-none input-bordered w-full "
+            />
+          </div>
+
+          <div className="flex gap-4">
+            <div className=" w-full">
+              <label className="label">
+                <span className="label-text">Category *</span>
+              </label>
+              <select
+                defaultValue={"Select a Category"}
+                {...register("category")}
+                className="focus:outline-none select select-bordered w-full "
+              >
+                <option disabled>Select a Category</option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="desserts">Desserts</option>
+                <option value="drinks">Drinks</option>
+                <option value="offered">Offered</option>
+              </select>
+            </div>
+
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text">Price *</span>
+              </label>
+              <input
+                {...register("price")}
+                type="number"
+                placeholder="Price"
+                className="input focus:outline-none input-bordered w-full "
+              />
+            </div>
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Recipe Details *</span>
+            </label>
+            <textarea
+              {...register("recipeDetails")}
+              className="textarea focus:outline-none textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </div>
           <input className="btn btn-secondary" type="submit" />
         </form>
       </div>
